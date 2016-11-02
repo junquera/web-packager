30c304f1098424cf0fb844f43e47b6e85c434c6c
Javier Junquera Sánchez <javier.junquera.sanchez@gmail.com>
2016-11-03 00:06:12 +0100

    Added methods

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const ugcss = require('uglifycss');
 
 var js_pattern = /<script.*src="([^"]*)"/g;
 var css_pattern = /<link.*href="([^"]*.css)"/g;
-var img_patter = /<img.*src="([^"]*)"/g;
+var img_pattern = /<img.*src="([^"]*)"/g;
 
 function getRegSults(text, regexp){
   var results = [];
@@ -13,3 +13,24 @@ function getRegSults(text, regexp){
     results.push(match[1])
   return results;
 }
+
+function readFile(filePath, callback){
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    callback(data);
+  });
+}
+
+readFile(process.argv[2], function(data){
+  // console.log(data);
+  getRegSults(data, js_pattern).forEach((x)=>console.log(x));
+  getRegSults(data, css_pattern).forEach((x)=>console.log(x));
+  getRegSults(data, img_pattern).forEach((x)=>console.log(x));
+});
+
+// const testFolder = './tests/';
+// const fs = require('fs');
+// fs.readdir(testFolder, (err, files) => {
+//   files.forEach(file => {
+//     console.log(file);
+//   });
+// })
